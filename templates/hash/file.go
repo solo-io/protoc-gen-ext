4b39bc1337ebee59194943382ae1e4e8b7de8b74
Conrad Hanson <27842017+conradhanson@users.noreply.github.com>
2022-12-06 14:23:49 -0800

    non-generated changes from external branch

--- templates/hash/file.go
+++ templates/hash/file.go
@@ -12,7 +12,7 @@ import (
 	"hash"
 	"hash/fnv"
 
-	"github.com/solo-io/protoc-gen-ext/pkg/hasher/hashstructure"
+	"github.com/mitchellh/hashstructure"
 	safe_hasher "github.com/solo-io/protoc-gen-ext/pkg/hasher"
 
 	{{ range $path, $pkg := enumPackages (externalEnums .) }}
